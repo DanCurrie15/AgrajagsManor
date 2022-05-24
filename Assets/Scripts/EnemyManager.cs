98d7b79336812547137d8607ac8096754ba7999e
Maple Mana Studios Inc <danjc_15@hotmail.com>
2022-05-23 22:58:44 -0400

    GameOver & Attacks
    -lamp attack is working
    -human and bookshelf needs work
    -triggering of enemy animation needs work
    -added game over screen and retry button

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,15 +7,36 @@ public class EnemyManager : Singleton<EnemyManager>
     public Transform spawnPoint;
     public GameObject enemy;
 
+    public List<GameObject> enemies = new List<GameObject>();
+
     private float spawnRate = 2f;
     private float nextSpawn = 0f;
 
     void Update()
     {
-        if (Time.time > nextSpawn)
+        if (Time.time > nextSpawn && GameManager.Instance.GameOn)
         {
             nextSpawn = Time.time + spawnRate;
             Instantiate(enemy, spawnPoint.position, Quaternion.identity);
         }
+    }
+
+    public GameObject GetClosestEnemy(Transform obj)
+    {
+        GameObject nearest = null;
+        float smallestDistance = float.PositiveInfinity;
+        if (enemies.Count < 1)
+        {
+            return null;
+        }
+        foreach (GameObject enemy in enemies)
+        {
+            if (Vector3.Distance(enemy.transform.position, obj.position) < smallestDistance)
+            {
+                smallestDistance = Vector3.Distance(enemy.transform.position, obj.position);
+                nearest = enemy;
+            }
+        }
+        return nearest;
     }
 }
